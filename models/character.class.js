6f593212a3ab6b6ec3ebcbcfc5a8303eda68fb9d
RichardPeda <richard.peda@gmx.net>
2024-04-16 22:43:01 +0200

    character idle animation

--- models/character.class.js
+++ models/character.class.js
@@ -23,6 +23,20 @@ class Character extends MovableObject {
         'img/2_character_pepe/1_idle/idle/I-9.png',
         'img/2_character_pepe/1_idle/idle/I-10.png',
     ];
+
+    IMAGES_LONG_IDLE = [
+        'img/2_character_pepe/1_idle/long_idle/I-11.png',
+        'img/2_character_pepe/1_idle/long_idle/I-12.png',
+        'img/2_character_pepe/1_idle/long_idle/I-13.png',
+        'img/2_character_pepe/1_idle/long_idle/I-14.png',
+        'img/2_character_pepe/1_idle/long_idle/I-15.png',
+        'img/2_character_pepe/1_idle/long_idle/I-16.png',
+        'img/2_character_pepe/1_idle/long_idle/I-17.png',
+        'img/2_character_pepe/1_idle/long_idle/I-18.png',
+        'img/2_character_pepe/1_idle/long_idle/I-19.png',
+        'img/2_character_pepe/1_idle/long_idle/I-20.png',
+    ];
+
     IMAGES_JUMPING = [
         'img/2_character_pepe/3_jump/J-31.png',
         'img/2_character_pepe/3_jump/J-32.png',
@@ -52,6 +66,10 @@ class Character extends MovableObject {
     collectedCoins = 0;
     collectedBottles = 0;
     audioCache = {};
+    isMoving = false;
+
+    lastMovement = 0;
+    timeStampSet = false;
 
     constructor() {
         super().loadImage(this.IMAGES_IDLE[0]);
@@ -64,6 +82,7 @@ class Character extends MovableObject {
         this.energy = 100;
         this.loadImages(this.IMAGES_WALK);
         this.loadImages(this.IMAGES_IDLE);
+        this.loadImages(this.IMAGES_LONG_IDLE);
         this.loadImages(this.IMAGES_JUMPING);
         this.loadImages(this.IMAGES_HURT);
         this.loadImages(this.IMAGES_DEAD);
@@ -77,9 +96,14 @@ class Character extends MovableObject {
     animate() {
         setStoppableInterval(() => {
             this.audioWalk.muted = true;
-            if (!this.isDead())
+            if (!this.isDead()) {
                 if (this.canMoveRight()) this.moveRight();
                 else if (this.canMoveLeft()) this.moveLeft();
+
+                if (this.canWalk() || this.canJump()) this.wakeUp()
+                else this.fallAsleep();
+            }
+
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
@@ -95,8 +119,26 @@ class Character extends MovableObject {
             else if (this.isAboveGround()) this.playAnimation(this.IMAGES_JUMPING);
             else if (this.canJump()) this.jump();
             else if (this.canWalk()) this.playAnimation(this.IMAGES_WALK);
-            else this.loadImage(this.IMAGES_IDLE[0]);
+            else if (this.isSleeping()) this.playAnimation(this.IMAGES_LONG_IDLE);
+            else this.playAnimation(this.IMAGES_IDLE);
         }, this.animationSpeed);
+    }
+
+    fallAsleep() {
+        if (!this.timeStampSet) {
+            this.lastMovement = new Date().getTime();
+            this.timeStampSet = true;
+        }
+    }
+
+    wakeUp() {
+        this.timeStampSet = false;
+    }
+
+    isSleeping() {
+        let timepassed = new Date().getTime() - this.lastMovement;
+        timepassed = timepassed / 1000;
+        return timepassed > 5;
     }
 
     /**
@@ -186,8 +228,8 @@ class Character extends MovableObject {
 
     /**
      * Increase the amount of collected bottles
-     * @param {Number} amount 
-     * @returns 
+     * @param {Number} amount
+     * @returns
      */
     collectBottle(amount) {
         return (this.collectedBottles += amount);
